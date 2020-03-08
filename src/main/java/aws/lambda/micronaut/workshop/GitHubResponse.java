7f855b595458a267af783d25821f4023147880d2
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-03-08 16:44:56 +0100

    Add GitHub service (#2)
    * Add GitHub service that requests username
    
    * Add github environment variable
    
    * Update github config
    
    * Replace config with '@Value'
    
    * Fix graalvm reflection
    
    Co-authored-by: hhoreis <9387550+henrik432@users.noreply.github.com>

---
+++ src/main/java/aws/lambda/micronaut/workshop/GitHubResponse.java
@@ -0,0 +1,25 @@
+package aws.lambda.micronaut.workshop;
+
+import io.micronaut.core.annotation.Introspected;
+import lombok.Data;
+
+@Data
+@Introspected
+public class GitHubResponse {
+
+    private GitHubData data;
+
+    public String getUsername() {
+        return data.getViewer().getLogin();
+    }
+
+    @Data
+    private static class GitHubData {
+        private Viewer viewer;
+    }
+
+    @Data
+    private static class Viewer {
+        private String login;
+    }
+}
---
+++ src/main/java/aws/lambda/micronaut/workshop/GitHubService.java
@@ -0,0 +1,30 @@
+package aws.lambda.micronaut.workshop;
+
+import io.micronaut.context.annotation.Prototype;
+import io.micronaut.context.annotation.Value;
+
+import javax.inject.Inject;
+
+@Prototype
+public class GitHubService {
+
+    private static final String USER_QUERY = "{\n" +
+            "viewer {\n" +
+            "login\n" +
+            "}\n" +
+            "}";
+
+    @Inject
+    public GitHubClient gitHubClient;
+
+    @Value("${github.token}")
+    public String gitHubToken;
+
+    @Value("${github.userAgent}")
+    public String userAgent;
+
+    public String getUsername() {
+        String authorization = "Bearer " + gitHubToken;
+        return gitHubClient.fetchUsername(authorization, userAgent, USER_QUERY).getUsername();
+    }
+}
