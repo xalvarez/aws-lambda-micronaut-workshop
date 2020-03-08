bf325e673c09e864c7a675bba2029710e97caabb
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-03-08 14:16:16 +0100

    Switch to JDK 11

--- sam-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker build . -t aws-lambda-micronaut-workshop
-mkdir -p build
-docker run --rm --entrypoint cat aws-lambda-micronaut-workshop  /home/application/function.zip > build/function.zip
-
-sam local start-api -t sam.yaml -p 3000
-
