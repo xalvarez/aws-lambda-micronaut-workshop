fc002b6cb0ea8aa163d1cef6eecf743442902d8f
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-03-29 15:57:40 +0200

    Add GitHub integration test

--- src/test/java/aws/lambda/micronaut/workshop/AbstractRestIntegrationTest.java
+++ src/test/java/aws/lambda/micronaut/workshop/AbstractRestIntegrationTest.java
@@ -2,11 +2,12 @@ package aws.lambda.micronaut.workshop;
 
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.annotation.Client;
+
 import javax.inject.Inject;
 
-public class AbstractRestIntegrationTest {
+public abstract class AbstractRestIntegrationTest extends AbstractWireMockTest {
 
-  @Inject
-  @Client("/")
-  protected RxHttpClient client;
+    @Inject
+    @Client("/")
+    protected RxHttpClient client;
 }
--- src/test/java/aws/lambda/micronaut/workshop/ExampleControllerRestIntegrationTest.java
+++ src/test/java/aws/lambda/micronaut/workshop/ExampleControllerRestIntegrationTest.java
@@ -1,21 +1,56 @@
 package aws.lambda.micronaut.workshop;
 
-import static javax.ws.rs.core.Response.Status.OK;
-import static org.assertj.core.api.BDDAssertions.then;
-
 import io.micronaut.test.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.assertj.core.api.BDDAssertions.then;
 
 @MicronautTest
 public class ExampleControllerRestIntegrationTest extends AbstractRestIntegrationTest {
 
-  @Test
-  public void shouldReplyPongToPing() {
-    String givenExpectedResponseBody = "{\"pong\":true, \"graal\": true}";
+    @Test
+    public void shouldReplyPongToPing() {
+        String givenExpectedResponseBody = "{\"pong\":true, \"graal\": true}";
 
-    var response = client.toBlocking().exchange("/ping", String.class);
+        var response = client.toBlocking().exchange("/ping", String.class);
 
-    then(response.getStatus().getCode()).isEqualTo(OK.getStatusCode());
-    then(response.body()).isEqualTo(givenExpectedResponseBody);
-  }
+        then(response.getStatus().getCode()).isEqualTo(OK.getStatusCode());
+        then(response.body()).isEqualTo(givenExpectedResponseBody);
+    }
+
+    @Test
+    public void shouldReturnGitHubUsername() {
+        givenSuccessfulGitHubtRequest();
+        String givenExpectedResponseBody = "{\"greeting\": \"Hello exampleUsername!\"}";
+
+        var response = client.toBlocking().exchange("/hello-github", String.class);
+
+        then(response.getStatus().getCode()).isEqualTo(OK.getStatusCode());
+        then(response.body()).isEqualTo(givenExpectedResponseBody);
+    }
+
+    private void givenSuccessfulGitHubtRequest() {
+        wireMockServer.stubFor(
+                post(urlPathEqualTo("/graphql")).willReturn(
+                        aResponse()
+                                .withStatus(OK.getStatusCode())
+                                .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                                .withBody(buildValidResponseBody())
+                )
+        );
+    }
+
+    private String buildValidResponseBody() {
+        return "{\n" +
+                "  \"data\": {\n" +
+                "    \"viewer\": {\n" +
+                "      \"login\": \"exampleUsername\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+    }
 }
