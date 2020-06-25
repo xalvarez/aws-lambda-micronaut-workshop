a0505ec3e74eb00986417737c43c58cda4a89d23
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-06-25 15:07:21 +0200

    Refactoring

--- src/main/java/aws/lambda/micronaut/workshop/restcountries/RestCountriesService.java
+++ src/main/java/aws/lambda/micronaut/workshop/restcountries/RestCountriesService.java
@@ -4,7 +4,6 @@ import static io.micronaut.http.HttpStatus.NOT_FOUND;
 
 import io.micronaut.context.annotation.Prototype;
 import io.micronaut.http.exceptions.HttpStatusException;
-import java.util.List;
 import java.util.Optional;
 import javax.inject.Inject;
 
@@ -15,18 +14,9 @@ public class RestCountriesService {
   public RestCountriesClient restCountriesClient;
 
   public String getCapital(String country) {
-
-    var countries = Optional.ofNullable(restCountriesClient.fetchCountry(country))
-        .orElse(List.of());
-
-    return countries
-        .stream()
-        .findFirst()
+    return Optional.ofNullable(restCountriesClient.fetchCountry(country))
+        .flatMap(restCountriesResponses -> restCountriesResponses.stream().findFirst())
         .map(RestCountriesResponse::getCapital)
-        .orElseThrow(() -> badRequestException(country));
-  }
-
-  private HttpStatusException badRequestException(String country) {
-    return new HttpStatusException(NOT_FOUND, "Couldn't find country " + country);
+        .orElseThrow(() -> new HttpStatusException(NOT_FOUND, "Couldn't find country " + country));
   }
 }
